83ff24bb1238e5196fe27d22bf9bf4d8719f4165
Yasik1397 <126237067+Yasik1397@users.noreply.github.com>
2023-08-23 17:23:02 +0530

    Update testRoutes.py

--- testRoutes.py
+++ testRoutes.py
@@ -2,5 +2,5 @@ from app import *
 
 def test_Home():
     response = app.test_client().get('/')
-    assert b"Welcome to Jenkins 12" in response.data
+    assert b"Welcome to Jenkins" in response.data
     assert response.status_code == 200
